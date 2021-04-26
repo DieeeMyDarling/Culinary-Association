a5d2be65afdfcfd7baa7e0bf00d97438cd4440f7
DieeeMyDarling <82842861+DieeeMyDarling@users.noreply.github.com>
2021-04-27 00:59:43 +0300

    Add files via upload

---
+++ archive/y.php
@@ -0,0 +1,9 @@
+<?php
+$("article a").click(function(e){
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500);
+    e.preventDefault(); //this is the important line.
+});
+?>
